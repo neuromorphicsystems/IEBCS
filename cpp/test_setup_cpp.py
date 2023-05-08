bac01e5ee233153fa177d43efcd187e061f3b5f2
joubert.damien@yahoo.fr <joubert.damien@yahoo.fr>
2023-05-08 13:58:54 +0000

    Remove ugly hard paths in module's tests

--- cpp/test_setup_cpp.py
+++ cpp/test_setup_cpp.py
@@ -7,16 +7,16 @@ sys.path.append("../src")
 from dvs_sensor import *
 import matplotlib.pyplot as plt
 
-dsi.initSimu(260, 346)
+dsi.initSimu(200, 200)
 dsi.initLatency(100.0, 30.0, 100.0, 1000.0)
 dsi.initContrast(0.3, 0.6, 0.035)
 init_bgn_hist_cpp("../data/noise_pos_3klux.npy", "../data/noise_pos_3klux.npy")
-path_img = "/home/joubertd/Documents/Data/Simulations/Comparison_ESIM/frames_book/"
-img = cv2.imread(path_img + 'frame_{}.png'.format(1606209988861033))
-img = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)[:, :, 0]
+img = np.zeros((200, 200), dtype=np.uint8)
+img[:, :] = 125
+img[50:150, 50:150] = 150
 dsi.initImg(img)
-img = cv2.imread(path_img + 'frame_{}.png'.format(1606209988909912))
-img = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)[:, :, 0]
+img[:, :] = 125
+img[55:155, 55:155] = 170
 s = dsi.updateImg(img, 46000)
 print(s)
 s = dsi.getShape()
