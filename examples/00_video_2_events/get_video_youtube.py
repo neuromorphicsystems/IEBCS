3aa652060dfa8d01dd919d9a8c2f34414efd12ab
joubert.damien@yahoo.fr <joubert.damien@yahoo.fr>
2023-05-08 14:13:23 +0000

    Update pytube call in example 00 to download the video

--- examples/00_video_2_events/get_video_youtube.py
+++ examples/00_video_2_events/get_video_youtube.py
@@ -1,6 +1,6 @@
 # From https://dev.to/seijind/how-to-download-youtube-videos-in-python-44od
 import pytube
 url = 'https://www.youtube.com/watch?v=RtUQ_pz5wlo'
-youtube = pytube.YouTube(url)
+youtube = pytube.YouTube(url, use_oauth=True, allow_oauth_cache=True)
 video = youtube.streams.get_highest_resolution()
 video.download("../../data/video/")
