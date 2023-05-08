ff22b92ccbec48bb82648f3462004fbb4b98f742
joubert.damien@yahoo.fr <joubert.damien@yahoo.fr>
2023-05-08 13:50:01 +0000

    Simplify setup of dsi packaging

--- cpp/setup_cpp.py
+++ cpp/setup_cpp.py
@@ -3,10 +3,10 @@ import numpy.distutils.misc_util
 
 c_ext = Extension("dsi",
                   sources=["simu_cpp.cpp"],
+                  language = "c++",
                   include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
                   extra_compile_args=['-std=c++17'])
 
-c_ext.language = 'c++'
 setup(
     name='dsi',
     version='1.0',
