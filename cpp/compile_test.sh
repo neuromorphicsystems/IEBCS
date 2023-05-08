61f57914683350451d8957f767b4908ab2eec495
joubert.damien@yahoo.fr <joubert.damien@yahoo.fr>
2023-05-08 14:40:31 +0000

    Install the DSI package in the right python environnement

--- cpp/compile_test.sh
+++ cpp/compile_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm dsi.cpython-37m-x86_64-linux-gnu.so
 python setup_cpp.py build_ext --inplace
-python setup_cpp.py install
+python setup_cpp.py install --home=$(which python | sed -e "s/\/bin\/python$//") 
 python test_setup_cpp.py 
  
