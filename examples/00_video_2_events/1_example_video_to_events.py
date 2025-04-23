5937a246af2989efc219a21ae88f908e80d9c380
glasspangolin <23103433+glasspangolin@users.noreply.github.com>
2025-04-23 17:19:28 +1000

    Cleaned out all outdated code, just leaving the python implementation which is confirmed to work without significant bug fixing.
    Several bugs have been fixed introduced by dependencies. Also pytube seems to no longer work, so I have moved to another youtube downloader.

--- examples/00_video_2_events/1_example_video_to_events.py
+++ examples/00_video_2_events/1_example_video_to_events.py
@@ -1,60 +1,95 @@
-# Joubert Damien, 03-02-2020
+# Joubert Damien, 03-02-2020 - updated by AvS 22-02-2024
 """
-    Script converting a video into events. The framerate of the video might not be the real framerate of the original video
-    . The user specifies this parameter at the beginning.
-    Please run get_video_youtube.py before
+    Script converting a video into events. 
+    The framerate of the video might not be the real framerate of the original video. 
+    The user specifies this parameter at the beginning.
+    Please run get_video_youtube.py before executing this script.
 """
 import cv2
 import sys
 sys.path.append("../../src")
 from event_buffer import EventBuffer
-from dvs_sensor import init_bgn_hist_cpp, DvsSensor
+from dvs_sensor import DvsSensor
 from event_display import EventDisplay
-import dsi
-import numpy as np
-filename = "../../data/video/See Hummingbirds Fly Shake Drink in Amazing Slow Motion  National Geographic.mp4"
-lat = 100
-jit = 10
-ref = 100
-tau = 300
-th = 0.3
-th_noise = 0.01
-cap = cv2.VideoCapture(filename)
-dsi.initSimu(int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
-dsi.initLatency(lat, jit, ref, tau)
-dsi.initContrast(th, th, th_noise)
-init_bgn_hist_cpp("../../data/noise_pos_161lux.npy", "../../data/noise_pos_161lux.npy")
-isInit = False
-dt = 1000  # FPS must be 1 kHz
-ev_full = EventBuffer(1)
-ed = EventDisplay("Events", cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT), dt*2)
+from arbiter import SynchronousArbiter, BottleNeckArbiter, RowArbiter
+from tqdm import tqdm
+import os
+
+if not os.path.exists("./outputs"):
+    os.mkdir("./outputs")
+
+filename = "../../data/hummingbird_video.mp4"
+th_pos = 0.4        # ON threshold = 50% (ln(1.5) = 0.4)
+th_neg = 0.4        # OFF threshold = 50%
+th_noise= 0.01      # standard deviation of threshold noise
+lat = 100           # latency in us
+tau = 40            # front-end time constant at 1 klux in us
+jit = 10            # temporal jitter standard deviation in us
+bgnp = 0.1          # ON event noise rate in events / pixel / s
+bgnn = 0.01         # OFF event noise rate in events / pixel / s
+ref = 100           # refractory period in us
+dt = 1000           # time between frames in us
 time = 0
-out = cv2.VideoWriter('video_{}_{}_{}_{}_{}_{}_nonoise.avi'.format(lat, jit, ref, tau, th, th_noise),
-    cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20.0, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-while cap.isOpened():
+
+cap = cv2.VideoCapture(filename)
+
+# Initialise the DVS sensor
+dvs = DvsSensor("MySensor")
+dvs.initCamera(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+                   lat=lat, jit = jit, ref = ref, tau = tau, th_pos = th_pos, th_neg = th_neg, th_noise = th_noise,
+                   bgnp=bgnp, bgnn=bgnn)
+# To use the measured noise distributions, uncomment the following line
+dvs.init_bgn_hist("../../data/noise_pos_161lux.npy", "../../data/noise_neg_161lux.npy")
+
+# Skip the first 50 frames of the video to remove video artifacts
+for i in range(50): 
     ret, im = cap.read()
-    out.write(im)
-    if im is None:
-        break
-    im = cv2.cvtColor(im, cv2.COLOR_RGB2LUV)[:, :, 0]
-    cv2.imshow("t", im)
-    cv2.waitKey(1)
-    if not isInit:
-        dsi.initImg(im)
-        isInit = True
-    else:
-        buf = dsi.updateImg(im, dt)
-        ev = EventBuffer(1)
-        ev.add_array(np.array(buf["ts"], dtype=np.uint64),
-                         np.array(buf["x"], dtype=np.uint16),
-                         np.array(buf["y"], dtype=np.uint16),
-                         np.array(buf["p"], dtype=np.uint64),
-                         100000000)
-        ed.update(ev, dt)
-        ev_full.increase_ev(ev)
-        time += dt
-        if time > 0.1e6:
+
+# Convert the image from uint8, such that 255 = 1e4, representing 10 klux
+im = cv2.cvtColor(im, cv2.COLOR_RGB2LUV)[:, :, 0] / 255.0 * 1e4
+
+# Set as the initial condition of the sensor
+dvs.init_image(im)
+
+# Create the event buffer
+ev_full = EventBuffer(1)
+
+# Create the arbiter - optional, pick from one below
+# ea = BottleNeckArbiter(0.01, time)                # This is a mock arbiter
+# ea = RowArbiter(0.01, time)                       # Old arbiter that handles rows in random order
+ea = SynchronousArbiter(0.1, time, im.shape[0])  # DVS346-like arbiter
+
+# Create the display
+render_timesurface = 1
+ed = EventDisplay("Events", 
+                  cap.get(cv2.CAP_PROP_FRAME_WIDTH), 
+                  cap.get(cv2.CAP_PROP_FRAME_HEIGHT), 
+                  dt, 
+                  render_timesurface)
+
+if cap.isOpened():
+    # Loop over num_frames frames
+    num_frames = 50
+    for frame in tqdm(range(num_frames), desc="Converting video to events"):
+        # Get frame from the video
+        ret, im = cap.read()
+        if im is None:
             break
-out.release()
+        # Convert the image from uint8, such that 255 = 1e4, representing 10 klux
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2LUV)[:, :, 0] / 255.0 * 1e4
+        # Calculate the events
+        ev = dvs.update(im, dt)
+        # Simulate the arbiter
+        # num_produced = ev.i
+        # ev = ea.process(ev, dt)
+        # num_released = ev.i
+        # statistics for the arbiter
+        # print("{} produced, {} released".format(num_produced, num_released))
+        # Display the events
+        ed.update(ev, dt)
+        # Add the events to the buffer for the full video
+        ev_full.increase_ev(ev)
+
 cap.release()
-ev_full.write('ev_{}_{}_{}_{}_{}_{}.dat'.format(lat, jit, ref, tau, th, th_noise))
+# Save the events to a .dat file
+ev_full.write('outputs/ev_{}_{}_{}_{}_{}_{}.dat'.format(lat, jit, ref, tau, th_pos, th_noise))
--- src/dat_files.py
+++ src/dat_files.py
@@ -1,27 +1,16 @@
 import numpy as np
-import pandas as pd
 import struct
-import matplotlib.pyplot as plt
-import cv2
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.font_manager import FontProperties
 from datetime import datetime
-import loris
-
-fontP = FontProperties()
-fontP.set_size('small')
-
 
 def load_dat_event(filename, start=0, stop=-1, display=False):
-    """ Load .dat event.
-        Warning: only tested on the VGA sensor on V2 Prophesee event
+    """ Load .dat events from file.
         Args:
             filename: Path of the .dat file
             start: starting timestamp (us)
             stop: if different than -1, last timestamp
-            display: display file infos
+            display: display file info
         Returns:
-             ts, x, y, pol numpy arrays of timestamps, position and polarities
+             ts, x, y, pol numpy arrays of timestamps, positions, and polarities
      """
     f = open(filename, 'rb')
     if f == -1:
@@ -36,9 +25,9 @@ def load_dat_event(filename, start=0, stop=-1, display=False):
         if display: print(l)
         l = f.readline()
         all_lines = all_lines + l
-    f.close()
+    # f.close()
     all_lines = str(all_lines)
-    f = open(filename, 'rb')
+    # f = open(filename, 'rb')
     f.seek(p, 0)
     evType = np.uint8(f.read(1)[0])
     evSize = np.uint8(f.read(1)[0])
@@ -62,7 +51,7 @@ def load_dat_event(filename, start=0, stop=-1, display=False):
     else:
         l_last = f.seek(0, 2)
 
-    num_b = (l_last - p) // evSize * 2
+    num_b = ((l_last - p) // int(evSize)) * 2
     f.close()
     data = np.fromfile(filename, dtype=np.uint32, count=num_b, offset=p)
     ts = data[::2]
@@ -91,7 +80,7 @@ def load_dat_event(filename, start=0, stop=-1, display=False):
     pol = data[1::2] & pol_mask
     pol = pol >> pol_shift
     if len(ts) > 0:
-        if display: 
+        if display:
             print("First Event: ", ts[0], " us")
             print("Last Event: ", ts[-1], " us")
             print("Number of Events: ", ts.shape[0])
@@ -111,10 +100,10 @@ def write_event_dat(filename, ts, x, y, pol,
     """
     f = open(filename, 'wb')
     if f == -1:
-        print("Impossible to open the file")
+        print("Cannot open the file")
         return
     if event_type in ['dvs', 'cd', 'td']:
-        f.write(bytes("% Data file containing CD events.\n", encoding='utf8'))
+        f.write(bytes("% Data file containing DVS events.\n", encoding='utf8'))
     elif event_type in ['aps', 'em']:
         f.write(bytes("% Data file containing EM events.\n", encoding='utf8'))
     else:
@@ -149,108 +138,6 @@ def write_event_dat(filename, ts, x, y, pol,
     arr.tofile(f)
     f.close()
 
-
-def write_event_es(filename, ts, x, y, pol,
-                   event_type='dvs', width=None, height=None):
-    """ Write the events in a .es file
-        Args:
-            filename: path of the file to create
-            ts: stimestamp
-            x, y: positions of the pixels
-            p: polarities (0 or 1)
-    """
-    events = np.recarray(shape=ts.shape,
-                         dtype=[('t', '<u8'), ('x', '<u2'),
-                                ('y', '<u2'), ('is_increase', '?')])
-    events['t'] = ts
-    events['x'] = x
-    events['y'] = y
-    events['is_increase'] = pol
-    if width is None:
-        width = x.max() + 1
-    if height is None:
-        height = y.max() + 1
-    file_dict = {'type': event_type, 'width': int(width), 'height': int(height), 'events': events}
-    loris.write_events_to_file(file_dict, filename)
-
-
-def write_event_csv(filename, ts, x, y, pol):
-    """ Write the events in a .csv file"""
-    df = pd.DataFrame({'ts': ts,
-                       'x': x,
-                       'y': y,
-                       'p': pol})
-
-    df = df.astype({'ts': '<u8', 'x': '<u2', 'y': '<u2', 'p': '?'})
-    df.to_csv(filename, header=True, index=False)
-
-
-def view_event(ts, x, y, pol, min_x=0, max_x=640, min_y=0, max_y=480, t_min=0, t_max=-1):
-    """ Visualise events in a 3d space
-        Time scale is in ms
-        Args:
-            ts, x, y, pol: events
-            min_y, max_y: Keeps events in [min_y max_y]
-            min_x, max_x: Keeps events in [min_x max_x]
-            t_min, t_max: Keeps events in [t_min t_max]
-    """
-    ind_pos = np.where(
-        (pol == 1) & (y > min_y) & (y < max_y) & (x > min_x) & (x < max_x) & (ts > t_min) & (ts < t_max))
-    ind_neg = np.where(
-        (pol == 0) & (y > min_y) & (y < max_y) & (x > min_x) & (x < max_x) & (ts > t_min) & (ts < t_max))
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x[ind_pos[0][:]], y[ind_pos[0][:]], ts[ind_pos[0][:]] / 1000, c='r', alpha=0.5, s=1)
-    ax.scatter(x[ind_neg[0][:]], y[ind_neg[0][:]], ts[ind_neg[0][:]] / 1000, c='g', alpha=0.5, s=1)
-    ax.set_xlabel('x (px)')
-    ax.set_ylabel('y (px) ')
-    #ax.set_zlabel(r' time ($\mu$s)')
-    ax.set_zlabel(r' time (ms)')
-    ax.set_xlim3d(min_x, max_x)
-    ax.set_ylim3d(min_y, max_y)
-    ax.set_zlim3d(t_min / 1000, t_max / 1000)
-    # ax.set_xticks(np.arange(min_y, max_y, 1))
-    # ax.set_yticks(np.arange(min_x, max_x, 1))
-    ax.set_proj_type('ortho')
-    plt.show()
-
-def make_video_event(ts, x, y, pol, min_x=0, max_x=640, min_y=0, max_y=480, t_min=0, t_max=-1, dt=100, filename="text.avi"):
-    """ Create a video of the events in a 3d space
-        Time scale is in ms
-        Args:
-            ts, x, y, pol: events
-            min_y, max_y: Keeps events in [min_y max_y]
-            min_x, max_x: Keeps events in [min_x max_x]
-            t_min, t_max: Keeps events in [t_min t_max]
-    """
-    fig = plt.figure()
-
-    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    out = cv2.VideoWriter(filename, fourcc, 10.0, (640, 480))
-    for t in range(int(t_min), int(t_max), int(dt)):
-        plt.clf()
-        ax = fig.add_subplot(111, projection='3d')
-        ind_pos = np.where(
-            (pol == 1) & (y > min_y) & (y < max_y) & (x > min_x) & (x < max_x) & (ts > t_min) & (ts < t))
-        ind_neg = np.where(
-            (pol == 0) & (y > min_y) & (y < max_y) & (x > min_x) & (x < max_x) & (ts > t_min) & (ts < t))
-
-        ax.scatter(x[ind_pos[0][:]], y[ind_pos[0][:]], ts[ind_pos[0][:]] / 1000, c='r', alpha=0.5, s=1)
-        ax.scatter(x[ind_neg[0][:]], y[ind_neg[0][:]], ts[ind_neg[0][:]] / 1000, c='g', alpha=0.5, s=1)
-        ax.set_xlabel('x (px)')
-        ax.set_ylabel('y (px) ')
-        #ax.set_zlabel(r' time ($\mu$s)')
-        ax.set_zlabel(r' time (ms)')
-        ax.set_xlim3d(min_x, max_x)
-        ax.set_ylim3d(min_y, max_y)
-        ax.set_zlim3d(t_min / 1000, t_max / 1000)
-        # ax.set_xticks(np.arange(min_y, max_y, 1))
-        # ax.set_yticks(np.arange(min_x, max_x, 1))
-        ax.set_proj_type('ortho')
-        plt.draw()
-        filename = './buf.png'
-        plt.savefig(filename)
-        im1 = cv2.imread(filename)
-        out.write(im1)
-    out.release()
-
+if __name__ == '__main__':
+    ts, x, y, pol =  load_dat_event("ev_100_10_100_300_0.3_0.01.dat", start=0, stop=-1, display=True)
+    print(ts.shape)
